9bd85c04d3de5c4d074e97d27702d0e60d577aee
anogin <ANogin@kazeuromobile.kz>
2023-10-15 13:21:00 +0600

    fixed war deploy

--- src/main/java/org/bookstore/repository/impl/BookRepositoryImpl.java
+++ src/main/java/org/bookstore/repository/impl/BookRepositoryImpl.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
-
 @Repository
 public class BookRepositoryImpl implements BookRepository {
     private final SessionFactory sessionFactory;
